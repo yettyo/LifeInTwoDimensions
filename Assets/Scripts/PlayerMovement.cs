838f4cc67e71d2857ea907d655f8b57cbcf5a1e5
yettyo <yenerkaraca12@gmail.com>
2023-05-22 11:53:46 +0300

    added hazards

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,16 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float runSpeed = 10f;
     [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(10f, 10f);
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform gun;
     Vector2 moveInput;
     Rigidbody2D rb;
     Animator anim;
     CapsuleCollider2D ccol;
     BoxCollider2D bcol;
     float gravityScaleAtStart;
+    bool isAlive = true;
 
     void Start()
     {
@@ -27,17 +31,26 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        if (!isAlive) { return; }
         Run();
         FlipSprite();
         ClimbLadder();
+        Die();
+    }
+
+    void OnFire(InputValue value) {
+        if (!isAlive) { return; }
+        Instantiate(bullet, gun.position, transform.rotation);
     }
 
     void OnMove(InputValue value) {
+        if (!isAlive) { return; }
         moveInput = value.Get<Vector2>();
         //Debug.Log(moveInput);
     }
 
     void OnJump(InputValue value) {
+        if (!isAlive) { return; }
         if (!bcol.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
         if (value.isPressed) {
             rb.velocity += new Vector2(0f, jumpSpeed);
@@ -71,5 +84,13 @@ public class PlayerMovement : MonoBehaviour
 
         bool playerHasVerticalSpeed = Mathf.Abs(rb.velocity.y) > Mathf.Epsilon;
         anim.SetBool("isClimbing", playerHasVerticalSpeed);
+    }
+
+    void Die() {
+        if (ccol.IsTouchingLayers(LayerMask.GetMask("Enemies", "Hazards"))) {
+            isAlive = false;
+            anim.SetTrigger("Dying");
+            rb.velocity = deathKick;
+        }
     }
 }
